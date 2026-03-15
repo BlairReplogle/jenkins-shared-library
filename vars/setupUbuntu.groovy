26d298119e58ead50a2cd1f59d4506e4f97421e9
Blair Replogle <BlairReplogle@users.noreply.github.com>
2026-03-15 11:35:36 -0600

    JSL-003: Add Setup Ubuntu

---
+++ vars/setupUbuntu.groovy
@@ -0,0 +1,19 @@
+//
+// setupHomebrew.groovy
+// Sets up homebrew
+//
+
+def call(Map config = [:]) {
+	println("Setting up Ubuntu...")
+
+	def usrLocalBin = "/usr/local/bin"
+	env.PATH = "${usrLocalBin}:${env.PATH}"
+
+	// Set auld LANG syne - put this somewhere else eventually... probably
+	env.LANG = "en_US.UTF-8"
+
+	// Check paths were added to the path
+	if ( !env.PATH.contains(usrLocalBin) ) {
+		error('Failed to configure environment for Ubuntu')
+	}
+}
